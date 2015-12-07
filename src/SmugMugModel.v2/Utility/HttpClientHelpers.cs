1c68675a530a3d72315220dfdf7b9d1953eeb74e
Alex Ghiondea <alex.ghiondea@gmail.com>
2015-12-06 23:52:54 -0800

    Minor refactoring to make the CreateHttpClient code easier to read

--- src/SmugMugModel.v2/Utility/HttpClientHelpers.cs
+++ src/SmugMugModel.v2/Utility/HttpClientHelpers.cs
@@ -10,12 +10,17 @@ namespace SmugMug.v2.Helpers
     {
         public static HttpClient CreateHttpClient(OAuthToken oauthToken)
         {
-            OAuth.OAuthMessageHandler _handler = new OAuth.OAuthMessageHandler(oauthToken.ApiKey, oauthToken.Secret, oauthToken.Token, oauthToken.TokenSecret);
+            OAuth.OAuthMessageHandler _handler = new OAuth.OAuthMessageHandler(
+                oauthToken.ApiKey,
+                oauthToken.Secret,
+                oauthToken.Token,
+                oauthToken.TokenSecret);
             HttpClient client = new HttpClient(_handler);
 
             // Make sure we request JSON data
             client.DefaultRequestHeaders.Accept.Clear();
-            client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+            client.DefaultRequestHeaders.Accept.Add(
+                new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
 
             return client;
         }
