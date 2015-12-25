f2f180090ee723979d15f624f1b5f11204f87a07
Alex Ghiondea <alex.ghiondea@gmail.com>
2015-12-24 22:20:55 -0800

    Add sample code to get the authenticated user from the site.

--- src/SmugMugTest.v2/Program.cs
+++ src/SmugMugTest.v2/Program.cs
@@ -1,5 +1,6 @@
 ﻿using SmugMug.v2.Authentication;
 using SmugMug.v2.Authentication.Tokens;
+using SmugMug.v2.Types;
 using System.Diagnostics;
 
 namespace SmugMugTest
@@ -13,7 +14,8 @@ namespace SmugMugTest
             s_oauthToken = ConsoleAuthentication.GetOAuthTokenFromProvider(new FileTokenProvider());
             Debug.Assert(!s_oauthToken.Equals(OAuthToken.Invalid));
 
-
+            SiteEntity site = new SiteEntity(s_oauthToken);
+            var user = site.GetAuthenticatedUserAsync().Result;
         }
     }
 }
