cc69ca1f4501078cef3ae6f36e6d843a5fc0fac5
Alex Ghiondea <alex.ghiondea@gmail.com>
2015-12-28 21:30:42 -0800

    Fill the ImageSizes methods

--- src/SmugMugModel.v2/Types/Entities/ImageSizes/ImageSizes.manual.cs
+++ src/SmugMugModel.v2/Types/Entities/ImageSizes/ImageSizes.manual.cs
@@ -10,124 +10,124 @@ namespace SmugMug.v2.Types
     public partial class ImageSizesEntity : SmugMugEntity
     {
 
-        public async Task<LargestImageEntity> Fixup_image____largestimage ()
+        public async Task<LargestImageEntity> GetLargestImageAsync()
         {
             // /image/(*)!largestimage 
-            return await image____largestimage(string.Empty); 
+            return await image____largestimage(ParentEntity.EntityId + "-0");
         }
 
-        public async Task<LargestVideoEntity> Fixup_image____largestvideo ()
+        public async Task<LargestVideoEntity> GetLargestVideoAsync()
         {
             // /image/(*)!largestvideo 
-            return await image____largestvideo(string.Empty); 
+            return await image____largestvideo(ParentEntity.EntityId + "-0");
         }
 
-        public async Task<ImageSizeCustomEntity> Fixup_image____sizecustom ()
+        public async Task<ImageSizeCustomEntity> GetImageSizeCustomAsync()
         {
             // /image/(*)!sizecustom 
-            return await image____sizecustom(string.Empty); 
+            return await image____sizecustom(ParentEntity.EntityId + "-0");
         }
 
-        public async Task<ImageSizeLargeEntity> Fixup_image____sizelarge ()
+        public async Task<ImageSizeLargeEntity> GetImageSizeLargeAsync()
         {
             // /image/(*)!sizelarge 
-            return await image____sizelarge(string.Empty); 
+            return await image____sizelarge(ParentEntity.EntityId + "-0");
         }
 
-        public async Task<ImageSizeMediumEntity> Fixup_image____sizemedium ()
+        public async Task<ImageSizeMediumEntity> GetImageSizeMediumAsync()
         {
             // /image/(*)!sizemedium 
-            return await image____sizemedium(string.Empty); 
+            return await image____sizemedium(ParentEntity.EntityId + "-0");
         }
 
-        public async Task<ImageSizeSmallEntity> Fixup_image____sizesmall ()
+        public async Task<ImageSizeSmallEntity> GetImageSizeSmallAsync()
         {
             // /image/(*)!sizesmall 
-            return await image____sizesmall(string.Empty); 
+            return await image____sizesmall(ParentEntity.EntityId + "-0");
         }
 
-        public async Task<ImageSizeThumbEntity> Fixup_image____sizethumb ()
+        public async Task<ImageSizeThumbEntity> GetImageSizeThumbAsync()
         {
             // /image/(*)!sizethumb 
-            return await image____sizethumb(string.Empty); 
+            return await image____sizethumb(ParentEntity.EntityId + "-0");
         }
 
-        public async Task<ImageSizeTinyEntity> Fixup_image____sizetiny ()
+        public async Task<ImageSizeTinyEntity> GetImageSizeTinyAsync()
         {
             // /image/(*)!sizetiny 
-            return await image____sizetiny(string.Empty); 
+            return await image____sizetiny(ParentEntity.EntityId + "-0");
         }
 
-        public async Task<ImageSizeX2LargeEntity> Fixup_image____sizex2large ()
+        public async Task<ImageSizeX2LargeEntity> GetImageSizeX2lLargeAsync()
         {
             // /image/(*)!sizex2large 
-            return await image____sizex2large(string.Empty); 
+            return await image____sizex2large(ParentEntity.EntityId + "-0");
         }
 
-        public async Task<ImageSizeX3LargeEntity> Fixup_image____sizex3large ()
+        public async Task<ImageSizeX3LargeEntity> GetImageSizeX3LargeAsync()
         {
             // /image/(*)!sizex3large 
-            return await image____sizex3large(string.Empty); 
+            return await image____sizex3large(ParentEntity.EntityId + "-0");
         }
 
-        public async Task<ImageSizeXLargeEntity> Fixup_image____sizexlarge ()
+        public async Task<ImageSizeXLargeEntity> GetImageSizeXLargeAsync()
         {
             // /image/(*)!sizexlarge 
-            return await image____sizexlarge(string.Empty); 
+            return await image____sizexlarge(ParentEntity.EntityId + "-0");
         }
 
-        public async Task<ImageSizeOriginalEntity> Fixup_image____sizeoriginal ()
+        public async Task<ImageSizeOriginalEntity> GetImageSizeOriginalAsync()
         {
             // /image/(*)!sizeoriginal 
-            return await image____sizeoriginal(string.Empty); 
+            return await image____sizeoriginal(ParentEntity.EntityId + "-0");
         }
 
-        public async Task<VideoSize110Entity> Fixup_video____size110 ()
+        public async Task<VideoSize110Entity> GetVideoSize110Async()
         {
             // /video/(*)!size110 
-            return await video____size110(string.Empty); 
+            return await video____size110(ParentEntity.EntityId + "-0");
         }
 
-        public async Task<VideoSize1280Entity> Fixup_video____size1280 ()
+        public async Task<VideoSize1280Entity> GetVideoSize1280Async()
         {
             // /video/(*)!size1280 
-            return await video____size1280(string.Empty); 
+            return await video____size1280(ParentEntity.EntityId + "-0");
         }
 
-        public async Task<VideoSize1920Entity> Fixup_video____size1920 ()
+        public async Task<VideoSize1920Entity> GetVideoSize1920Async()
         {
             // /video/(*)!size1920 
-            return await video____size1920(string.Empty); 
+            return await video____size1920(ParentEntity.EntityId + "-0");
         }
 
-        public async Task<VideoSize200Entity> Fixup_video____size200 ()
+        public async Task<VideoSize200Entity> GetVideoSize200Async()
         {
             // /video/(*)!size200 
-            return await video____size200(string.Empty); 
+            return await video____size200(ParentEntity.EntityId + "-0");
         }
 
-        public async Task<VideoSize320Entity> Fixup_video____size320 ()
+        public async Task<VideoSize320Entity> GetVideoSize320Async()
         {
             // /video/(*)!size320 
-            return await video____size320(string.Empty); 
+            return await video____size320(ParentEntity.EntityId + "-0");
         }
 
-        public async Task<VideoSize640Entity> Fixup_video____size640 ()
+        public async Task<VideoSize640Entity> GetVideoSize640Async()
         {
             // /video/(*)!size640 
-            return await video____size640(string.Empty); 
+            return await video____size640(ParentEntity.EntityId + "-0");
         }
 
-        public async Task<VideoSize960Entity> Fixup_video____size960 ()
+        public async Task<VideoSize960Entity> GetVideoSize960Async()
         {
             // /video/(*)!size960 
-            return await video____size960(string.Empty); 
+            return await video____size960(ParentEntity.EntityId + "-0");
         }
 
-        public async Task<VideoSMILEntity> Fixup_video____smil ()
+        public async Task<VideoSMILEntity> GetVideoSMILAsync()
         {
             // /video/(*)!smil 
-            return await video____smil(string.Empty); 
+            return await video____smil(ParentEntity.EntityId + "-0");
         }
     }
 }
