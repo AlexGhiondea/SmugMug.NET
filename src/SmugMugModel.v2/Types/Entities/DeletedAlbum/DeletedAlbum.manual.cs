2c89362638110ce06a3ff1704f5f301c5adaa0db
Alex Ghiondea <alex.ghiondea@gmail.com>
2015-12-28 21:31:45 -0800

    Fill the DeletedAlbum methods

--- src/SmugMugModel.v2/Types/Entities/DeletedAlbum/DeletedAlbum.manual.cs
+++ src/SmugMugModel.v2/Types/Entities/DeletedAlbum/DeletedAlbum.manual.cs
@@ -10,16 +10,16 @@ namespace SmugMug.v2.Types
     public partial class DeletedAlbumEntity : SmugMugEntity
     {
 
-        public async Task<RecoverDeletedAlbumEntity> Fixup_deletedalbum____recover ()
+        public async Task<RecoverDeletedAlbumEntity> RequiresPost_Fixup_deletedalbum____recover()
         {
             // /deleted/album/(*)!recover 
-            return await deletedalbum____recover(string.Empty); 
+            return await deletedalbum____recover(string.Empty);
         }
 
-        public async Task<UserEntity> Fixup_user___ ()
+        public async Task<UserEntity> GetUserAsync()
         {
             // /user/(*) 
-            return await user___(string.Empty); 
+            return await user___(ParentEntity.EntityId);
         }
     }
 }
