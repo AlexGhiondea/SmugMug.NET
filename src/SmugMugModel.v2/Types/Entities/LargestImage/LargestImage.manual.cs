f9f5eff6db5e6afd8b675ec2019458d09f3ba608
Alex Ghiondea <alex.ghiondea@gmail.com>
2015-12-28 21:31:21 -0800

    Fill the LargestImage methods

--- src/SmugMugModel.v2/Types/Entities/LargestImage/LargestImage.manual.cs
+++ src/SmugMugModel.v2/Types/Entities/LargestImage/LargestImage.manual.cs
@@ -10,22 +10,22 @@ namespace SmugMug.v2.Types
     public partial class LargestImageEntity : SmugMugEntity
     {
 
-        public async Task<ImageSizeX3LargeEntity> Fixup_image____sizex3large ()
+        public async Task<ImageSizeX3LargeEntity> GetImageSizeX3LargeAsync()
         {
             // /image/(*)!sizex3large 
-            return await image____sizex3large(string.Empty); 
+            return await image____sizex3large(ParentEntity.EntityId + "-0");
         }
 
-        public async Task<ImageSizeOriginalEntity> Fixup_image____sizeoriginal ()
+        public async Task<ImageSizeOriginalEntity> GetImageSizeOriginalAsync()
         {
             // /image/(*)!sizeoriginal 
-            return await image____sizeoriginal(string.Empty); 
+            return await image____sizeoriginal(ParentEntity.EntityId + "-0");
         }
 
-        public async Task<ImageSizeXLargeEntity> Fixup_image____sizexlarge ()
+        public async Task<ImageSizeXLargeEntity> GetImageSizeXLargeAsync()
         {
             // /image/(*)!sizexlarge 
-            return await image____sizexlarge(string.Empty); 
+            return await image____sizexlarge(ParentEntity.EntityId + "-0");
         }
     }
 }
