4d33ea152898882e8ea062d9307b65c5e8cbb016
Alex Ghiondea <alex.ghiondea@gmail.com>
2015-12-28 21:26:13 -0800

    Remove .manual.cs file from the project that don't have any code in them.

---
+++ src/SmugMugModel.v2/Utility/EnumExtensions.cs
@@ -0,0 +1,23 @@
+﻿// Copyright (c) Alex Ghiondea. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+using System.Runtime.Serialization;
+
+namespace SmugMug.v2.Utility
+{
+    public static class EnumExtensions
+    {
+        public static string GetEnumMemberValue(this Enum enumVal)
+        {
+            var enumType = enumVal.GetType();
+            var memInfo = enumType.GetMember(enumVal.ToString());
+            var attributes = memInfo[0].GetCustomAttributes(typeof(EnumMemberAttribute), false);
+
+            if (attributes.Length == 0)
+                return enumVal.ToString();
+
+            return (attributes[0] as EnumMemberAttribute).Value;
+        }
+    }
+}
