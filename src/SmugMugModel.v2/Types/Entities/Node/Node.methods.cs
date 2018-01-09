c57d1dc73bf8c30bf7ce9f838ed51b7f78b34725
Alex Ghiondea <alex.ghiondea@gmail.com>
2018-01-08 23:35:22 -0800

    Inline Entities methods.

--- src/SmugMugModel.v2/Types/Entities/Node/Node.methods.cs
+++ src/SmugMugModel.v2/Types/Entities/Node/Node.methods.cs
@@ -34,69 +34,12 @@ namespace SmugMug.v2.Types
 
         private static readonly List<string> PostParameters = new List<string>() { "AutoRename", "Description", "HideOwner", "HighlightImageUri", "Name", "Keywords", "Password", "PasswordHint", "Privacy", "SecurityType", "SmugSearchable", "SortDirection", "SortMethod", "Type", "UrlName", "WorldSearchable" };
 
-
-        private async Task<AlbumEntity> album___(string param1)
-        {
-            // /album/(*) 
-            string requestUri = string.Format("{0}/album/{1}", SmugMug.v2.Constants.Addresses.SmugMugApi, param1);
-
-            return await RetrieveEntityAsync<AlbumEntity>(requestUri);
-        }
-
-        private async Task<FolderEntity> folderid___(string param1)
-        {
-            // /folder/id/(*) 
-            string requestUri = string.Format("{0}/folder/id/{1}", SmugMug.v2.Constants.Addresses.SmugMugApi, param1);
-
-            return await RetrieveEntityAsync<FolderEntity>(requestUri);
-        }
-
-        private async Task<ImageEntity> highlightnode___(string param1)
-        {
-            // /highlight/node/(*) 
-            string requestUri = string.Format("{0}/highlight/node/{1}", SmugMug.v2.Constants.Addresses.SmugMugApi, param1);
-
-            return await RetrieveEntityAsync<ImageEntity>(requestUri);
-        }
-
-        private async Task<NodeEntity[]> node____children(string nodeId, string options)
-        {
-            // /node/(*)!children 
-            string requestUri = string.Format("{0}/node/{1}!children{2}", SmugMug.v2.Constants.Addresses.SmugMugApi, nodeId, string.IsNullOrEmpty(options) ? "" : $"?{options}");
-
-            return await RetrieveEntityArrayAsync<NodeEntity>(requestUri);
-        }
-
-        private async Task<GrantEntity[]> node____grants(string param1)
-        {
-            // /node/(*)!grants 
-            string requestUri = string.Format("{0}/node/{1}!grants", SmugMug.v2.Constants.Addresses.SmugMugApi, param1);
-
-            return await RetrieveEntityArrayAsync<GrantEntity>(requestUri);
-        }
-
         private async Task node____movenodes(string param1)
         {
             // /node/(*)!movenodes 
             string requestUri = string.Format("{0}/node/{1}!movenodes", SmugMug.v2.Constants.Addresses.SmugMugApi, param1);
 
             await GetRequestAsync(requestUri);
-        }
-
-        private async Task<NodeEntity> node____parent(string param1)
-        {
-            // /node/(*)!parent 
-            string requestUri = string.Format("{0}/node/{1}!parent", SmugMug.v2.Constants.Addresses.SmugMugApi, param1);
-
-            return await RetrieveEntityAsync<NodeEntity>(requestUri);
-        }
-
-        private async Task<NodeEntity[]> node____parents(string param1)
-        {
-            // /node/(*)!parents 
-            string requestUri = string.Format("{0}/node/{1}!parents", SmugMug.v2.Constants.Addresses.SmugMugApi, param1);
-
-            return await RetrieveEntityArrayAsync<NodeEntity>(requestUri);
         }
 
         private async Task<UserEntity> user___(string param1)
