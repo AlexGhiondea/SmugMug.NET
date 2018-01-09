2761106adc171be3b0637b60dfbe5dbda23c70d8
Alex Ghiondea <alex.ghiondea@gmail.com>
2018-01-08 23:39:06 -0800

    Cleanup Catalog methods

--- src/SmugMugModel.v2/Types/Catalog/CatalogProduct/CatalogProduct.methods.cs
+++ src/SmugMugModel.v2/Types/Catalog/CatalogProduct/CatalogProduct.methods.cs
@@ -19,38 +19,5 @@ namespace SmugMug.v2.Types
         {
             _oauthToken = oauthToken;
         }
-
-
-        private async Task<CatalogCategoryEntity> catalogcategory___ (string param1)
-        {
-            // /catalog/category/(*) 
-            string requestUri = string.Format("{0}/catalog/category/{1}", SmugMug.v2.Constants.Addresses.SmugMugApi, param1);
-
-            return await RetrieveEntityAsync<CatalogCategoryEntity>(requestUri); 
-        }
-
-        private async Task<CatalogSubCategoryEntity> catalog___prints___ (string param1, string param2)
-        {
-            // /catalog/(*)/prints/(*) 
-            string requestUri = string.Format("{0}/catalog/{1}/prints/{2}", SmugMug.v2.Constants.Addresses.SmugMugApi, param1,param2);
-
-            return await RetrieveEntityAsync<CatalogSubCategoryEntity>(requestUri); 
-        }
-
-        private async Task<CatalogSkuEntity[]> catalogproduct____options (string param1)
-        {
-            // /catalog/product/(*)!options 
-            string requestUri = string.Format("{0}/catalog/product/{1}!options", SmugMug.v2.Constants.Addresses.SmugMugApi, param1);
-
-            return await RetrieveEntityArrayAsync<CatalogSkuEntity>(requestUri); 
-        }
-
-        private async Task<CatalogVendorEntity> catalogvendor___ (string param1)
-        {
-            // /catalog/vendor/(*) 
-            string requestUri = string.Format("{0}/catalog/vendor/{1}", SmugMug.v2.Constants.Addresses.SmugMugApi, param1);
-
-            return await RetrieveEntityAsync<CatalogVendorEntity>(requestUri); 
-        }
     }
 }
